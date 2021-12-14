3671d48a7e823c06d3073df6564febc90ce0f924
Michal <marekmichal1991@gmail.com>
2021-12-14 19:57:26 +0100

    Cw3 zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -18,6 +14,25 @@ public class Main {
             d) wypisać wartości pól (za pomocą getterów),
             e) wywołać metody suma i róznica (obie wersje) z klasy punkt
         */
+        Punkt punktA = new Punkt();
+        punktA.setpX(7);
+        punktA.toString();
+
+        Punkt punkt = new Punkt(1,2,3);
+        punkt.toString();
+
+        punktA.setpX(3);
+        punktA.setpY(5);
+        punktA.setpZ(7);
+        punktA.toString();
+
+        System.out.println(punktA.getpX());
+        System.out.println(punktA.getpY());
+        System.out.println(punktA.getpZ());
+
+        System.out.println("Suma wszystkich punktów to: " + punktA.suma());
+        System.out.println("Różnica wszystkich punktów to: " + punktA.roznica());
+        System.out.println("Różnica wszystkich punktów przy x = 5, y=2, z=1 to: " + punktA.roznica(5,2,1));
 
     }
 }
